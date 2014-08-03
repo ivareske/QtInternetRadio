9a7ca250a4e6f570e6f57c03ce4115e3d52ebfeb
Ivar Eskerud Smith <ivar.eskerud@gmail.com>
2014-08-03 17:26:22 +0200

    .

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,101 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QMultimedia>
-#include <QMediaPlayer>
+#include <QMessageBox>
+#include <QFile>
+
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
+
+    QFile file(qApp->applicationDirPath()+"/presets.xml");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QMessageBox::critical(this,"Error","Could not open presets.xml",QMessageBox::Ok);
+        return;
+    }
+
+    QXmlStreamReader xml(&file);
+    xml.readNextStartElement();
+    while (!xml.atEnd()) {
+        qDebug()<<xml.name();
+
+        QString group = xml.attributes().value("title").toString();
+        qDebug()<<group;
+        if(!group.isEmpty() && xml.attributes().hasAttribute("id")){
+            QMenu *menu = new QMenu(group);
+            ui->menuPresets->addMenu(menu);
+            xml.readNextStartElement();
+            addPresetSubMenus(xml,menu);
+        }else{
+            xml.readNextStartElement();
+        }
+
+    }
+}
+
+bool MainWindow::isStation(const QXmlStreamReader &xml) const{
+    return xml.name().toString().toLower()=="station";
+}
+
+void MainWindow::addPresetSubMenus( QXmlStreamReader &xml, QMenu *parent ){
+
+    bool isEndElement = xml.isEndElement();
+    if(isEndElement){
+        if(xml.readNextStartElement()){
+            addPresetSubMenus(xml,parent);
+        }
+    }
+
+    qDebug()<<xml.attributes().value("title").toString()<<xml.name().toString();
+    bool _isStation = isStation(xml);
+
+    if(!_isStation && !isEndElement){
+        //The child group also has child groups, add those recursively
+        QString title = xml.attributes().value("title").toString();
+        //qDebug()<<title;
+        QMenu *menu = new QMenu(title);
+        parent->addMenu(menu);
+        xml.readNextStartElement();
+        addPresetSubMenus(xml,menu);
+    }else if(_isStation){
+        //This child group does not have more child group, add it as as action
+        //qDebug()<<xml.name().toString();
+
+        QString url = xml.attributes().value("url").toString();
+        QString name = xml.attributes().value("title").toString();
+        bool ok;
+        int id = xml.attributes().value("id").toInt(&ok);
+        if(!ok){
+            id=-1;
+        }
+        if(xml.readNextStartElement()){
+            QStringList sources;
+            QString tmpname = xml.name().toString().toLower();
+            while(tmpname=="source"){
+                QString source = xml.readElementText();
+                sources.append(source);
+                xml.readNextStartElement();
+                tmpname = xml.name().toString().toLower();
+            }
+            //xml.readNextStartElement();
+            Station station(name,url,sources,id);
+            _stations[id]=station;
+            QAction *action = new QAction(name,parent);
+            action->setData(id);
+            connect(action,SIGNAL(triggered()),this,SLOT(presetTriggered()));
+            parent->addAction(action);
+
+
+            addPresetSubMenus(xml,parent);
+        }
+    }
+
+
+
+
+}
+
+void MainWindow::presetTriggered(){
+
 }
 
 MainWindow::~MainWindow(){
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,47 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QXmlStreamReader>
+#include <QMultimedia>
+#include <QMediaPlayer>
+#include <QStringList>
 
 namespace Ui {
 class MainWindow;
 }
+
+
+
+class Station{
+
+public:
+
+    explicit Station(const QString &name="", const QString &url="",const QStringList &sources=QStringList(),const int &id=-1){
+        _name=name;
+        _url=url;
+        _sources=sources;
+        _id=id;
+    }
+
+    inline QString name() const{
+        return _name;
+    }
+    inline QString url() const{
+        return _url;
+    }
+    inline QStringList sources() const{
+        return _sources;
+    }
+    inline int id() const{
+        return _id;
+    }
+
+private:
+    QString _name,_url;
+    QStringList _sources;
+    int _id;
+};
+
 
 class MainWindow : public QMainWindow{
     Q_OBJECT
@@ -19,8 +56,15 @@ private slots:
     void on_StopButton_clicked();
     void on_MuteButton_clicked();
 
+    void presetTriggered();
+
 private:
+    bool isStation(const QXmlStreamReader &xml) const;
+    void addPresetSubMenus(QXmlStreamReader &xml, QMenu *parent);
+
+
     Ui::MainWindow *ui;
+    QHash<int,Station> _stations;
 
 };
 
